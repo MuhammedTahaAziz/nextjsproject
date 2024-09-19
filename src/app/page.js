01fbc4f5377dd364244864cb164cfe226b6e40bb
MuhammedTahaAziz <muhammed.t.aziz@gmail.com>
2024-09-18 23:52:50 -0700

    ready to response

--- src/app/page.js
+++ src/app/page.js
@@ -5,15 +5,15 @@ import Description from "@/components/Description";
 import Destination from "@/components/Destination";
 import Navbar from "@/components/Navbar";
 import Services from "@/components/Services";
-import useOpenStore from "@/lib/useOpenStore";
+// import useOpenStore from "@/lib/useOpenStore";
 import { useEffect } from "react";
 
 export default function Home() {
-  const { isOpen, setOpen } = useOpenStore();
+  // const { isOpen, setOpen } = useOpenStore();
 
-  useEffect(() => {
-    setOpen(false);
-  }, [setOpen]);
+  // useEffect(() => {
+  //   setOpen(false);
+  // }, [setOpen]);
 
   return (
     <div className="w-10/12 h-full mx-auto pt-20  scrollbar-hide"
